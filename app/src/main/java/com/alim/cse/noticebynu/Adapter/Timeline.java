1ec226b6b0d10b9218531a26acecb270e119a211
Hacker-0 <sourav.0.alim@gmail.com>
2020-01-05 20:38:11 +0600

    No Changes

---
+++ app/src/main/java/com/alim/cse/noticebynu/Adapter/Timeline.java
@@ -0,0 +1,24 @@
+package com.alim.cse.noticebynu.Adapter;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Timeline extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/alim/cse/noticebynu/Fragment/TimelineFragment.java
@@ -0,0 +1,18 @@
+package com.alim.cse.noticebynu.Fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.alim.cse.noticebynu.R;
+
+public class TimelineFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timeline, container, false);
+    }
+}
